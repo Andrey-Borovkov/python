96a26dbb9aa9ce3c6dc4bc16b7c4bd7b19796733
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-07-14 13:04:37 +0300

    add Task1

--- lec.py
+++
@@ -1,8 +0,0 @@
-print('Введите цифру от 1 до 7')
-a = int(input())
-if not (a in range(1, 8)):
-    print('Введенная цифра не из диапозона от 1 до 7')
-elif a in range(1, 6):
-    print('Будний день')
-else:
-    print('Выходной день')
